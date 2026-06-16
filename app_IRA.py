55c8517a90986ae2614b66f0eeb35b1b6aab6988
Irano-616 <filhojadir261@gmail.com>
2026-06-16 10:36:00 -0300

    Update app_IRA.py

--- app_IRA.py
+++ app_IRA.py
@@ -27,7 +27,7 @@ for i in range(6):
     with col_nota:
         nota = st.number_input("", min_value=0.0, max_value=100.0, value=0.0, step=10.0, key=f"n_{i}", label_visibility="collapsed")
     with col_cred:
-        cred = st.number_input("", min_value=0, value=0, key=f"c_{i}", label_visibility="collapsed")
+        cred = st.number_input("", min_value=0, max_value=10, value=0, key=f"c_{i}", label_visibility="collapsed")
         
     soma_pontos += nota * cred
     soma_creditos += cred
