ed740595c1a304c6080c70ce117881f6ea69f3f8
Irano-616 <filhojadir261@gmail.com>
2026-06-16 10:06:10 -0300

    Update app_nota.py

--- app_nota.py
+++ app_nota.py
@@ -5,8 +5,8 @@ st.header("Coloque sua média nas respectivas caixas de texto")
 
 st.space() # O espaço entre o st.header e as variaveis n1 e n2
 
-n1 = st.number_input("Coloque a sua nota N1: ", step = None) 
-n2 = st.number_input("Coloque a sua nota N2: ", step = None)
+n1 = st.number_input("Coloque a sua nota N1: ", step=None) 
+n2 = st.number_input("Coloque a sua nota N2: ", step=None)
 
 media = ((n1+n2)/2)/10
 
